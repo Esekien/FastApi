c273514cc996cc12d542f4466d18a05a4eface3f
carlos <carlos_wow55@hotmail.com>
2022-04-12 03:47:59 -0500

    update

--- app.py
+++ app.py
@@ -2,6 +2,15 @@ from fastapi import FastAPI
 from routes.users import user
 from routes.problems import problems
 
-app = FastAPI()
+app = FastAPI(
+    openapi_tags=[{
+        "name": "users",
+        "description": "users routes"
+    },
+    {
+        "name": "problems",
+        "description": "problems routes"
+    }]
+)
 app.include_router(user)
 app.include_router(problems)
--- routes/problems.py
+++ routes/problems.py
@@ -10,7 +10,7 @@ lista = [['0','0','0','0'],
 
 
 
-@problems.get('/par-impar',response_model=List[int], tags=['problems'])
+@problems.get('/par-impar',response_model=List[int], tags=['problems'], description='Envia una lista y retorna dos lista una con par y otra impar')
 def get_list(lista: List[int] = Query(None)):
     lista.sort()
     pares = []
@@ -22,7 +22,7 @@ def get_list(lista: List[int] = Query(None)):
             impares.append(i)
     return {'pares':pares , 'impares':impares}
 
-@problems.post('/problema2', tags=['problems'])
+@problems.post('/problema2', tags=['problems'], description='Ingresa la fila y la columna a ocupar y se mostrara las butacas ocupadas')
 def post_butaca(fila:int, colum:int):
     if fila > 4 or colum > 3:
         return 'fila o columna invalida(s)'
